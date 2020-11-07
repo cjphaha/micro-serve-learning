bfb87455198ccaa150e0663f5ee4ced0cb797558
cjp <cjp_ca@163.com>
2020-11-07 21:39:28 +0800

    加了一个装饰器
    在微服务01里面的prod_client里面，每次访问接口都会自动执行一个东西，有点像切片

--- 微服务01/prod_main.go
+++ 微服务01/prod_main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"github.com/micro/go-micro"
+	"github.com/micro/go-micro/client"
 	"github.com/micro/go-micro/registry"
 	"github.com/micro/go-micro/web"
 	"github.com/micro/go-plugins/registry/consul"
@@ -13,17 +16,29 @@ import (
 商品API
 */
 
+type logWrapper struct {
+	client.Client
+}
+//重写call方法
+func(o *logWrapper)Call(ctx context.Context, req client.Request, rsp interface{},pots ...client.CallOption) error{
+	fmt.Println("调用接口")
+	o.Client.Call(ctx,req,rsp)
+	return nil
+}
+
+func NewLogWrapper(c client.Client) client.Client{
+	return &logWrapper{c}
+}
 func main() {
 	consulReg := consul.NewRegistry(//初始化consul
 		registry.Addrs("localhost:8500"), //consul运行的地址
 	)
-
 	//商品服务,这里就是rpc相关的了
-
 	myService := micro.NewService(
 		micro.Name("Prodservew2134143.client"),
 		micro.Address(":8012"),
 		micro.Registry(consulReg),//注册到consul
+		micro.WrapClient(NewLogWrapper),//注册装饰器
 		)
 	prodService := Services.NewProdService("Prodserve",myService.Client())
 	httpServer := web.NewService(//对应于http，这是在consul里面注册的过程
